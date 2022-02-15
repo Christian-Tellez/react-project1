df5363e4af12365bfa8fdeff0bbb87ba3d6f04cc
Christian <christian.tellez.giraldo@gmail.com>
2022-02-15 21:09:26 +0100

    After section 5.68

--- src/App.js
+++ src/App.js
@@ -2,45 +2,42 @@ import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-const App = () => {
-  let expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
-  const [enteredExpenses, setEnteredExpenses] = useState(expenses);
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
     console.log("In App.js: ");
     console.log(expense);
 
-    setEnteredExpenses((prevState) => {
-      return [
-        ...prevState,
-        expense,
-      ];
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
     });
   };
 
@@ -48,7 +45,7 @@ const App = () => {
     <div>
       <h2>Let's get started!</h2>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={enteredExpenses} />
+      <Expenses items={expenses} />
     </div>
   );
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,45 +1,46 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
-const Expenses = (props) => {
+const Expenses = ({ items }) => {
   const [selectedFilter, setSelectedFilter] = useState("2020");
-  const [selectedItems, setSelectedItems] = useState(
-    props.items.filter((item) => {
-      return item.date.getFullYear().toString() === selectedFilter;
-    })
-  );
 
   const filterChangeHandler = (selectedYear) => {
     setSelectedFilter(selectedYear);
-    setSelectedItems(
-      props.items.filter((item) => {
-        return item.date.getFullYear().toString() === selectedYear;
-      })
-    );
   };
 
+  const fiteredExpenses = items.filter((expense) => {
+    return expense.date.getFullYear().toString() === selectedFilter;
+  });
+
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (fiteredExpenses.length > 0) {
+    expensesContent = fiteredExpenses.map((expense) => {
+      return (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      );
+    });
+  }
+
   return (
-    <Fragment>
+    <li>
       <Card className="expenses">
         <ExpensesFilter
           selected={selectedFilter}
           onChangeFilter={filterChangeHandler}
         />
-        {selectedItems.map((value, index) => {
-          return (
-            <ExpenseItem
-              key={selectedItems[index].id}
-              title={selectedItems[index].title}
-              amount={selectedItems[index].amount}
-              date={selectedItems[index].date}
-            />
-          );
-        })}
+        <ExpensesList items={fiteredExpenses} />
       </Card>
-    </Fragment>
+    </li>
   );
 };
 
